59d31afa46d5036847309aa1bb2faea1a525d1e1
sosukesuzuki <aosukeke@gmail.com>
2021-07-15 16:27:19 +0900

    Implement babel-plugin-proposal-await-ops

--- packages/babel-plugin-proposal-await-ops/src/index.ts
+++ packages/babel-plugin-proposal-await-ops/src/index.ts
@@ -1,17 +1,35 @@
 import { declare } from "@babel/helper-plugin-utils";
 import syntaxAwaitOps from "@babel/plugin-syntax-await-ops";
-import type * as t from "@babel/types";
+import * as t from "@babel/types";
 import type { NodePath } from "@babel/traverse";
 
 export default declare(api => {
-  api.assertVersion("^7.15.0");
+  api.assertVersion("^7.14.5");
   return {
     name: "proposal-await-ops",
     inherits: syntaxAwaitOps,
     visitor: {
       AwaitExpression: {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        exit(path: NodePath<t.AwaitExpression>) {},
+        exit(path: NodePath<t.AwaitExpression>) {
+          const { node } = path;
+          if (!node.operation) {
+            return;
+          }
+          const operationName = node.operation.name;
+          const operationPath = path.get("operation");
+          operationPath.remove();
+          const argument = node.argument;
+          const argumentPath = path.get("argument");
+          argumentPath.replaceWith(
+            t.callExpression(
+              t.memberExpression(
+                t.identifier("Promise"),
+                t.identifier(operationName),
+              ),
+              [argument],
+            ),
+          );
+        },
       },
     },
   };
